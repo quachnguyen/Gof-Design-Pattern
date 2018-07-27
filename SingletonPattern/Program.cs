4b8044ed84f95436690a0b040ab990bd22e8c4cc
nguyen.quach <nguyen.quach@sutrixsolutions.com>
2018-07-27 15:13:42 +0700

    Initial and added singleton pattern demo project

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Singleton demo");
+            Console.WriteLine("create instance 1");
+            var instance1 = SingletonPatternEx.Instance;
+            Console.WriteLine("create instance 2");
+            var instance2 = SingletonPatternEx.Instance;
+
+            if (instance1 == instance2)
+                Console.WriteLine("Only one object is instance");
+            else
+                Console.WriteLine("Different instance exist");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonPattern/SingletonPatternEx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    public sealed class SingletonPatternEx
+    {
+        private static readonly SingletonPatternEx instance = new SingletonPatternEx();
+        private static int totalInstance = 0;
+        private SingletonPatternEx()
+        {
+            Console.WriteLine("Instance object via private constructor");
+            totalInstance++;
+            Console.WriteLine($"Number of instance {totalInstance}");
+        }
+
+        public static SingletonPatternEx Instance
+        {
+            get{
+                Console.WriteLine("exist and we will reuse it!");
+                return instance;
+            }
+        }
+    }
+}
